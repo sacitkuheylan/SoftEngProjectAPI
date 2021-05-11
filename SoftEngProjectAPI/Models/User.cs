6923a6c75b77f70dd378df26852a7664053f09dd
SACIT KUHEYLAN <sacit.kuheylan@yandex.com>
2021-05-12 01:38:38 +0300

    Initial Commit

---
+++ SoftEngProjectAPI/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoftEngProjectAPI.Models
+{
+    public class User
+    {
+        public int userID { get; set; }
+
+        public string name { get; set; }
+
+        public string surname { get; set; }
+
+        public string email { get; set; }
+
+        public string password { get; set; }
+    }
+}
---
+++ SoftEngProjectAPI/Models/UserDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoftEngProjectAPI.Models
+{
+    public class UserDBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public UserDBContext(DbContextOptions<UserDBContext> options) : base(options)
+        {
+
+        }
+    }
+}
